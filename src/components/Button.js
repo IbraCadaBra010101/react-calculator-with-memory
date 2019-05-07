e0c276f70dd690f41cd5b21b4f37d17e2ae8aa95
IbraCadaBra010101 <ibma010101@gmail.com>
2019-05-07 19:50:08 +0200

    working on input and previous state callback

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 
 class Button extends Component {
+    number = () => {
+       this.props.buttonHandler(this.props.text);
+    };
+
     render() {
 
-        return (<button className={'calc-button'}>{this.props.text}</button>)
+        return (<button className={'calc-button'} onClick={this.number}>{this.props.text}</button>)
     }
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,26 +1,25 @@
 import React, {Component} from 'react';
-import Button from './Button'
 
 class Screen extends Component {
 
     render() {
-      //  console.log(this.props.state.expression);
-        let buttonsReused = [];
-        let symbols = ['+', '-', '*', '/', 'MR', 'MS'];
-        for (let i = 0; i < 10; i++) {
-            buttonsReused.push(<Button key ={i} text={i}/>);
-            console.log(buttonsReused)
-        }
-        for (let symbol of symbols) {
-            buttonsReused.push(<Button key ={symbol} text={symbol}/>)
-        }
+
         return (<React.Fragment>
-            <input type="text" value={this.props.value}/>
-            {buttonsReused}
+            <input type="text" value={this.props.expression}/>
         </React.Fragment>)
     }
 }
 
+
 export default Screen;
 
-
+// handleClick = event => {
+//     this.setState( (prevState, props) => {
+//             // returns a new state object
+//             return { counter: prevState.counter + 1 };
+//         },
+//         () => {
+//             // runs after changed state
+//         }  // callback
+//     );  // setState
+// }
