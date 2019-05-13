152aed9837e94e85c8715f2aaed10dcb43aa5973
IbraCadaBra010101 <ibma010101@gmail.com>
2019-05-13 14:41:59 +0200

    almost there

--- src/Calculate.js
+++ src/Calculate.js
@@ -9,112 +9,110 @@ class Calculate extends Component {
         input: '',
         current: 0,
         result: 0,
+        soFar: 0,
         all: [],
         showAll: false,
         additionMarker: false,
         subtractionMarker: false,
-        markingColor: 'green'
+        markingColor: 'green',
+        operatorClicked: 0,
     };
-
-
-    // Jag förstår. Nej, det ska inte behövas några
-    // callbacks. Om du sparat resultatet av den senaste
-    // plus/minus operationen och det senast inskrivna värdet i state, så är det bara att kombinera dem.
-    //
-    // Rendera resultatet när man klickar på "=". När man skriver en
-    // längre operation (1+2-3+4 osv) så vill jag se delresultatet
-    // (t.ex. i input-fältet) varje gång man klickar på plus/minus.
-
-
-    operatorClicked = 0;
     handleChange = (event) => {
         this.setState({
             input: event,
         });
     };
     addition = () => {
+        let operatorWasClicked = this.state.operatorClicked;
 
         this.setState({
             operator: '+',
             additionMarker: true,
             subtractionMarker: false,
-            current: this.state.input,
+            current: this.state.current + Number(this.state.input),
+            result: this.state.current,
+            operatorClicked: this.state.operatorClicked + 1,
         });
-        console.log(this.state)
-    };
 
+        if (operatorWasClicked > 0) {
+            this.setState({
+                input: this.state.current + Number(this.state.input)
+            })
+        }
+    };
     subtraction = () => {
+        let operatorWasClicked = this.state.operatorClicked;
         this.setState({
             operator: '-',
             additionMarker: false,
             subtractionMarker: true,
             current: this.state.input,
+            result: this.state.current,
+            operatorClicked: this.state.operatorClicked + 1,
         });
+        if (operatorWasClicked > 0) {
+            this.setState({
+                input: Number(this.state.current) - Number(this.state.input)
+            })
+        }
     };
     clear = () => {
         this.setState({
             input: 0,
         })
     };
-
     equals = () => {
         if (this.state.operator === '+') {
-            let result =Number(this.state.current) + Number(this.state.input);
             this.setState({
-                current: result,
-                result: result + Number(this.state.input)
+                result: Number(this.state.input) + this.state.current,
+                current: Number(this.state.input) + this.state.current
             });
         }
         if (this.state.operator === '-') {
-            let result =+(Number(this.state.current) - Number(this.state.input));
-            console.log(result);
             this.setState({
-                current: this.state.current - Number(this.state.input),
-                result:  result - Number(this.state.input),
+                result: this.state.current - Number(this.state.input),
+                current: this.state.current - Number(this.state.input)
             });
         }
-
     };
-savedResults = [];
-addToMemory = () => {
-    this.savedResults.push(this.state.current);
-};
-latestNum = 0;
-useMemory = () => {
-    const toBeUsedInExpression = [...this.savedResults];
-    this.latestNum = toBeUsedInExpression.pop();
-    this.setState({
-        current: this.latestNum - this.latestNum,
-        input: this.latestNum,
-    });
-};
+    savedResults = [];
+    addToMemory = () => {
+        this.savedResults.push(this.state.input);
+    };
+    latestNum = 0;
+    useMemory = () => {
+        const toBeUsedInExpression = [...this.savedResults];
+        this.latestNum = toBeUsedInExpression.pop();
+        this.setState({
+            input: this.latestNum,
+        });
+    };
 
-render()
-{
-    return <React.Fragment>
-        <h3>Result: {this.state.result}</h3>
-        <h3>Current:{this.state.current}</h3>
-        <Screen input={this.state.input}
-                result={this.state.result}
-                current={this.state.current}
-                handleChange={this.handleChange}
-                operatorClicked={this.operatorClicked}
-                all={this.state.all}
-        />
-        <OperandButtons addition={this.addition}
-                        subtraction={this.subtraction}
-                        clear={this.clear}
-                        equals={this.equals}
-                        additionMarker={this.state.additionMarker}
-                        subtractionMarker={this.state.subtractionMarker}
-                        markingColor={this.state.markingColor}
-        />
-        <CalcMemory addToMemory={this.addToMemory}
-                    useMemory={this.useMemory}
+    render() {
+        return <React.Fragment>
+            <h3>Result: {this.state.result}</h3>
+            <h3>Current:{this.state.current}</h3>
+            <Screen input={this.state.input}
+                    result={this.state.result}
+                    current={this.state.current}
+                    handleChange={this.handleChange}
+                    operatorClicked={this.operatorClicked}
                     all={this.state.all}
-        />
-    </React.Fragment>
-}
+            />
+            <OperandButtons addition={this.addition}
+                            subtraction={this.subtraction}
+                            clear={this.clear}
+                            equals={this.equals}
+                            additionMarker={this.state.additionMarker}
+                            subtractionMarker={this.state.subtractionMarker}
+                            markingColor={this.state.markingColor}
+            />
+            <CalcMemory addToMemory={this.addToMemory}
+                        useMemory={this.useMemory}
+                        all={this.state.all}
+            />
+        </React.Fragment>
+    }
 }
 
 export default Calculate;
